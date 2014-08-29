b9b720822e0340c3ad96b183e9ebca7406bc9d5e
mrgnmok <mrgnmok@gmail.com>
2014-08-29 14:50:50 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,25 @@
+///////////////////
+// Morgan Mok
+// mwm217
+// CSE 2 Lab 01
+// Hello World Java Program
+
+//  Define public class
+
+public class HelloWorld{
+//  Add main method
+    public static void main (String[] args) {
+        
+//  print statement within box
+        System.out.println("####################################");
+        System.out.println("####################################");
+        System.out.println("#                                  #");
+        System.out.println("#                                  #");
+        System.out.println("#       Hellow World 2014!!        #");
+        System.out.println("#                                  #");
+        System.out.println("#                                  #");
+        System.out.println("####################################");
+        System.out.println("####################################");
+        System.out.println("test");
+    }
+}
