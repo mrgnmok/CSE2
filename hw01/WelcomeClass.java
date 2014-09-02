659d3bbd3a7d62b7beea4a7dea6b662617362c57
mrgnmok <mrgnmok@gmail.com>
2014-09-02 23:22:56 +0000

    compiled and finalized hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,30 @@
+// Morgan Mok
+// mwm217
+// CSE2
+// Professor Brian Chen
+// Homework 1
+//////////////////
+
+//define public class 
+
+public class WelcomeClass {
+//  add main method
+    public static void main (String[] args) {
+//  Print username within box with a tweet-biography at the end. Text will appear as such.
+        System.out.println("-----------");
+        System.out.println("           ");
+        System.out.println("| WELCOME |");
+        System.out.println("           ");
+        System.out.println("-----------");
+        System.out.println("           ");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-M--W--M--2--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("This summer I went to Europe and saw various historical sites with family. 10/10 would recommend.");
+        
+}
+    }
+    
+    
