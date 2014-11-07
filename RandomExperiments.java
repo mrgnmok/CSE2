06e42b05fbd8162f9acfc8bc9d386feb0ba8b30e
mrgnmok <mrgnmok@gmail.com>
2014-11-07 04:15:46 +0000

    work

--- RandomExperiments.java
+++ RandomExperiments.java
@@ -5,24 +5,25 @@ import java.util.Scanner;
 public class RandomExperiments {
 public static void main (String [] args) {
     
-    int nyumber=0;
-    Scanner myScanner = new Scanner(System.in);
-    
-    System.out.println("Enter your name");
-        String name = myScanner.next();
-            System.out.println("Your name is "+name+".");
-    System.out.println("Enter your birthday in a sequence of numbers mmddyyyy. Eg. 03121990");
-        int birthdy = myScanner.nextInt();
-    System.out.println("do you kill people? Have you thought about killing people? Yy/Nn");
-        String answer = myScanner.next();
-        if (answer =='Y' || answer=='y') {
-            nyumber++;
-        }
-        else if (answer == 'N' || answer == 'n') {
-            nyumber--;
-        }
-        else System.out.println ("You didn't enter a valid answer. Goodbye.");
-    
-    System.out.println ("based on your answers, you should be a hufflepuff.");
-        }
+//NESTED LOOP PRACTICE:
+//WRITE A LOOP THAT JUST SHOWS ALL THE NUMBERS UNTIL 10.
+
+Scanner myScanner = new Scanner(System.in);
+System.out.println ("Enter a number from 1-10");
+
+int userInput = myScanner.nextInt();
+int i=0;
+
+do {
+    System.out.println(i);
+    i++;
 }
+    while (i<=userInput);
+    }
+    
+}
+
+
+
+
+
